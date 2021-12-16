6c100147f71c702322b8fa2c87693efba2dcb532
Sadzobu <49387865+Sadzobu@users.noreply.github.com>
2021-12-16 13:07:42 +0300

    Rails introduction 1 (#1)
    * added rails_introduction_1 features
    
    * style changes
    
    * corrected url tags
    
    * corrected url tags
    
    * changed version according to semver
    
    * changed version initializer and url's behavior
    
    * changed version functionality
    
    * yet another version reshuffle, now in initializers again

--- TestGuru/config/application.rb
+++ TestGuru/config/application.rb
@@ -7,6 +7,11 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module TestGuru
+
+  #def self.version
+  #  '0.1.0'
+  #end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -15,7 +20,9 @@ module TestGuru
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
