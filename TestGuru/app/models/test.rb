15b9144931d70402ba3f46fdfd19abef0f5aad9b
Sadzobu <kaonokage@gmail.com>
2022-01-27 23:56:51 -0800

    added dependencies to all models, changed version

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -2,14 +2,11 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
-  has_many :questions
+  has_many :questions, dependent: :destroy
   has_many :test_completions
-  has_many :users, through: :test_completions
+  has_many :users, through: :test_completions, dependent: :destroy
 
   def self.tests_by_category_title_desc(title)
-    joins('JOIN categories ON tests.categories_id = categories.id')
-      .where(categories: { title: title })
-      .order(title: :desc)
-      .pluck(:title)
+    joins(:category).where(categories: { title: title }).order(title: :desc).pluck(:title)
   end
 end
