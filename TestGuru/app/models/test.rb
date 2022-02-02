86be57327dc4f90419c1b6589b37b90f0bec55c5
Sadzobu <kaonokage@gmail.com>
2022-02-02 01:40:16 -0800

    merged tests title validations, style change

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -6,9 +6,8 @@ class Test < ApplicationRecord
   has_many :test_completions, dependent: :destroy
   has_many :users, through: :test_completions
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :title, uniqueness: { scope: :level }
 
   scope :by_level, ->(level) { where(level: level) }
   scope :by_category, ->(title) { joins(:category).where(categories: { title: title }) }
