6d51963b43511355d215d100cc890d0ed2119522
Sadzobu <49387865+Sadzobu@users.noreply.github.com>
2022-02-03 09:46:43 +0300

    Rails validations (#8)
    * added scopes and redefined methods using scopes
    
    * added verifications
    
    * added db level unique constaint on tests [title, level] pair, corrected small mistakes
    
    * merged tests title validations, style change

--- TestGuru/app/models/answer.rb
+++ TestGuru/app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  validate  :answer_count, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def answer_count
+    errors.add(:base, 'Question can only have up to 4 answers!') if question.answers.count >= 4
+  end
 end
--- TestGuru/db/seeds.rb
+++ TestGuru/db/seeds.rb
@@ -7,9 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-categories = Category.create!([{ title: 'Backend' }, { title: 'Frontend' }, { title: 'Machine Learning' }])
+categories = Category.create!([{ title: 'Backend' },
+                               { title: 'Frontend' },
+                               { title: 'Machine Learning' }])
 
-users = User.create!([{ name: 'Admin_1' }, { name: 'Admin_2' }, { name: 'User_1' }, { name: 'User_2' }])
+users = User.create!([{ name: 'Admin_1', email: 'admin_1@ya.ru' },
+                      { name: 'Admin_2', email: 'admin_2@ya.ru' },
+                      { name: 'User_1', email: 'user_1@ya.ru' },
+                      { name: 'User_2', email: 'user_2@ya.ru' }])
 
 tests = Test.create!([{ title: 'Ruby 0 0', level: 0, category: categories[0], author: users[0] },
                       { title: 'Ruby 1 1', level: 1, category: categories[1], author: users[0] },
@@ -37,8 +42,8 @@ Answer.create!([{ body: 'Ruby 0 0 correct answer', correct: true, question: ques
                 { body: 'Ruby 1 1 incorrect answer', correct: false, question: questions[2] },
                 { body: 'Ruby 1 1 incorrect answer', correct: false, question: questions[3] },
                 { body: 'Python 0 0 incorrect answer', correct: false, question: questions[4] },
-                { body: 'Python 1 2 incorrect answer', correct: false, question: questions[5] },
-                { body: 'HTML 1 0 incorrect answer', correct: false, question: questions[6] }])
+                { body: 'Python 1 2 incorrect answer', correct: false, question: questions[0] },
+                { body: 'HTML 1 0 incorrect answer', correct: false, question: questions[0] }])
 
 
 TestCompletion.create!([{ user: users[2], test: tests[1] },
