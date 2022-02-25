9cc1467230211d3c9ecda0df6a5943976eb039f3
Sadzobu <kaonokage@gmail.com>
2022-02-25 03:46:28 -0800

    added user authenticate logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_completions, dependent: :destroy
   has_many :tests, through: :test_completions
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-
-  validates :name, presence: true
-  validates :email, presence: true
 
   def tests_completed_by_level(level)
     tests.by_level(level)
