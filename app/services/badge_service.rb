05314a8947a983f2d9d5fea351eb6f031f5f7945
Sadzobu <kaonokage@gmail.com>
2022-05-26 03:20:13 -0700

    changed rule_passed? logic, to perform selection using db

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -26,15 +26,33 @@ class BadgeService
   end
 
   def passed_all_in_level_rule?(level)
-    @user.tests.by_level(level).uniq.map(&:passed?).count == Tests.by_level(level).count
+    number_of_tests_in_level = Test.by_level(level).count
+
+    TestCompletion.where(passed: true)
+                  .where(user_id: @user.id)
+                  .joins(:test)
+                  .where(tests: { level: level })
+                  .uniq
+                  .count == number_of_tests_in_level
   end
 
   def passed_all_in_category_rule?(category)
-    @user.tests.by_category(category).uniq.map(&:passed?).count == Tests.by_category(category).count
+    number_of_tests_in_category = Test.by_category(category).count
+    category_id = Category.find_by(title: category).id
+
+    TestCompletion.where(passed: true)
+                  .where(user_id: @user.id)
+                  .joins(:test)
+                  .where(tests: { category_id: category_id })
+                  .uniq
+                  .count == number_of_tests_in_category
   end
 
-  def passed_first_time_rule?(title)
-    @user.test_completions.count { |x| x.test.title == title } == 1
+  def passed_first_time_rule?(id)
+    TestCompletion.where(user_id: @user.id)
+                  .joins(:test)
+                  .where(tests: { id: id })
+                  .count == 1
   end
 
 end
